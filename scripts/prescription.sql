cf8e3a8b6ed74c9c1887a4d46b76bd318003dfaf
Aimee Johnson <aimeejohnson@NSS-ADMINs-MacBook-Pro.local>
2024-06-12 11:41:19 -0500

    join exercises up to question 6

--- scripts/prescription.sql
+++ scripts/prescription.sql
@@ -73,18 +73,57 @@ LIMIT 1;
 
 -- 4. 
 --     a. For each drug in the drug table, return the drug name and then a column named 'drug_type' which says 'opioid' for drugs which have opioid_drug_flag = 'Y', says 'antibiotic' for those drugs which have antibiotic_drug_flag = 'Y', and says 'neither' for all other drugs. **Hint:** You may want to use a CASE expression for this. See https://www.postgresqltutorial.com/postgresql-tutorial/postgresql-case/ 
-SELECT *
+SELECT drug_name,
+CASE
+	WHEN opioid_drug_flag = 'Y' THEN 'opioid'
+	WHEN antibiotic_drug_flag = 'Y' THEN 'antibiotic'
+	ELSE 'neither'
+END AS drug_type
 FROM drug;
+
 --     b. Building off of the query you wrote for part a, determine whether more was spent (total_drug_cost) on opioids or on antibiotics. Hint: Format the total costs as MONEY for easier comparision.
+SELECT SUM(total_drug_cost) :: Money AS total_cost, 
+CASE
+	WHEN opioid_drug_flag = 'Y' THEN 'opioid'
+	WHEN antibiotic_drug_flag = 'Y' THEN 'antibiotic'
+	ELSE 'neither'
+END AS drug_type
+FROM drug
+INNER JOIN prescription
+USING (drug_name)
+GROUP BY drug_type
+ORDER BY total_cost DESC;
+
+--Opioids
 
 -- 5. 
 --     a. How many CBSAs are in Tennessee? **Warning:** The cbsa table contains information for all states, not just Tennessee.
+SELECT COUNT(*)
+FROM cbsa
+WHERE cbsaname LIKE '%TN';
 
+-- 33
 --     b. Which cbsa has the largest combined population? Which has the smallest? Report the CBSA name and total population.
+SELECT cbsaname, SUM(population) AS total_population
+FROM cbsa
+INNER JOIN population
+USING (fipscounty)
+GROUP BY cbsaname
+ORDER BY total_population DESC;
 
 --     c. What is the largest (in terms of population) county which is not included in a CBSA? Report the county name and population.
+SELECT county, population
+FROM fips_county
+INNER JOIN population
+USING (fipscounty)
+FULL JOIN cbsa
+USING (fipscounty)
+WHERE cbsa IS NULL
+ORDER BY population DESC;
 
--- 6. 
+--"SEVIER"	95523
+
+-- 6.
 --     a. Find all rows in the prescription table where total_claims is at least 3000. Report the drug_name and the total_claim_count.
 
 --     b. For each instance that you found in part a, add a column that indicates whether the drug is an opioid.
@@ -98,4 +137,3 @@ FROM drug;
 --     b. Next, report the number of claims per drug per prescriber. Be sure to include all combinations, whether or not the prescriber had any claims. You should report the npi, the drug name, and the number of claims (total_claim_count).
     
 --     c. Finally, if you have not done so already, fill in any missing values for total_claim_count with 0. Hint - Google the COALESCE function.
-
