c836de8435fcc2a9e2ec62e69e59782191da5b5b
Aimee Johnson <aimeejohnson@NSS-ADMINs-MacBook-Pro.local>
2024-06-15 11:11:33 -0500

    bonus questions completed

--- scripts/bonus_questions.sql
+++ scripts/bonus_questions.sql
@@ -1,9 +1,18 @@
 -- 1. How many npi numbers appear in the prescriber table but not in the prescription table?
-SELECT npi
+SELECT COUNT(*)
+FROM(
+	SELECT npi
+	FROM prescriber
+	EXCEPT
+	SELECT npi
+	FROM prescription
+) as not_prescription;
+
+SELECT COUNT(prescriber.npi)
 FROM prescriber
-EXCEPT
-SELECT npi
-FROM prescription;
+LEFT JOIN prescription
+ON prescriber.npi = prescription.npi
+WHERE prescription.npi IS NULL;
 
 -- 2.
 --     a. Find the top five drugs (generic_name) prescribed by prescribers with the specialty of Family Practice.
@@ -68,14 +77,92 @@ LIMIT 5;
 
 -- 3. Your goal in this question is to generate a list of the top prescribers in each of the major metropolitan areas of Tennessee.
 --     a. First, write a query that finds the top 5 prescribers in Nashville in terms of the total number of claims (total_claim_count) across all drugs. Report the npi, the total number of claims, and include a column showing the city.
-    
+SELECT prescriber.npi, prescription.total_claim_count, prescriber.nppes_provider_city
+FROM prescriber
+INNER JOIN prescription
+USING (npi)
+WHERE nppes_provider_city = 'NASHVILLE'
+ORDER BY total_claim_count DESC;
+
+SELECT prescriber.npi, SUM(prescription.total_claim_count) as total_claims, prescriber.nppes_provider_city
+FROM prescriber
+INNER JOIN prescription
+USING (npi)
+WHERE nppes_provider_city = 'NASHVILLE'
+GROUP BY prescriber.npi,prescriber.nppes_provider_city 
+ORDER BY total_claims DESC;
+
 --     b. Now, report the same for Memphis.
+SELECT prescriber.npi, prescription.total_claim_count, prescriber.nppes_provider_city
+FROM prescriber
+INNER JOIN prescription
+USING (npi)
+WHERE nppes_provider_city = 'MEMPHIS'
+ORDER BY total_claim_count DESC;
+
+SELECT prescriber.npi, SUM(prescription.total_claim_count) as total_claims, prescriber.nppes_provider_city
+FROM prescriber
+INNER JOIN prescription
+USING (npi)
+WHERE nppes_provider_city = 'MEMPHIS'
+GROUP BY prescriber.npi,prescriber.nppes_provider_city 
+ORDER BY total_claims DESC;
     
 --     c. Combine your results from a and b, along with the results for Knoxville and Chattanooga.
+SELECT prescriber.npi, prescription.total_claim_count, prescriber.nppes_provider_city
+FROM prescriber
+INNER JOIN prescription
+USING (npi)
+WHERE nppes_provider_city IN ('NASHVILLE,''MEMPHIS', 'KNOXVILLE', 'CHATTANOOGA')
+ORDER BY total_claim_count DESC;
+
+SELECT prescriber.npi, SUM(prescription.total_claim_count) as total_claims, prescriber.nppes_provider_city
+FROM prescriber
+INNER JOIN prescription
+USING (npi)
+WHERE nppes_provider_city IN ('NASHVILLE,''MEMPHIS', 'KNOXVILLE', 'CHATTANOOGA')
+GROUP BY prescriber.npi,prescriber.nppes_provider_city 
+ORDER BY total_claims DESC;
 
 -- 4. Find all counties which had an above-average number of overdose deaths. Report the county name and number of overdose deaths.
 
+-- SELECT fipscounty, AVG(o.overdose_deaths) AS avg_overdose_deaths
+-- FROM overdose_deaths o
+-- GROUP BY o.fipscounty;
+
+WITH a AS(SELECT o.fipscounty, AVG(o.overdose_deaths) AS avg_overdose_deaths
+FROM overdose_deaths o
+GROUP BY o.fipscounty)
+SELECT f.county, o.overdose_deaths
+FROM overdose_deaths o
+LEFT JOIN fips_county f
+ON o.fipscounty = f.fipscounty::INTEGER
+JOIN a
+ON o.fipscounty = a.fipscounty
+WHERE o.overdose_deaths > a.avg_overdose_deaths
+ORDER BY overdose_deaths DESC;
+
 -- 5.
 --     a. Write a query that finds the total population of Tennessee.
-    
+SELECT f.state,SUM(p.population) as total_population
+FROM population p
+	INNER JOIN fips_county f
+ON p.fipscounty = f.fipscounty
+WHERE f.state = 'TN'
+GROUP BY f.state;
+
 --     b. Build off of the query that you wrote in part a to write a query that returns for each county that county's name, its population, and the percentage of the total population of Tennessee that is contained in that county.
+WITH tn AS (
+    SELECT SUM(p.population) AS total_population
+    FROM population p
+    INNER JOIN fips_county f
+    	ON p.fipscounty = f.fipscounty
+    WHERE f.state = 'TN'
+)
+SELECT f.county,p.population, 
+    (p.population / tn.total_population) * 100 AS county_percent
+FROM population p
+JOIN fips_county f
+USING (fipscounty)
+CROSS JOIN tn
+WHERE f.state = 'TN';
